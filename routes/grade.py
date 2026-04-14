985a8278ced166e831767d5a2e7200743d563c1a
gfalvo1968-create <gfalvo1968@icloud.com>
2026-04-14 01:17:44 -0400

    Update grade.py

--- routes/grade.py
+++ routes/grade.py
@@ -1,1 +1,13 @@
+from fastapi import APIRouter
+from model.classifier import predict_board_grade
 
+router = APIRouter()
+
+@router.post("/predict")
+def predict():
+    # TEMP: use a test image
+    image_path = "data/images/IMG_0772.jpeg"
+
+    result = predict_board_grade(image_path)
+
+    return {"prediction": result}
