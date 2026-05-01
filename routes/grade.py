e5d75015bc0a1d19e1c22a330f3714bbfaa53bc8
gfalvo1968-create <gfalvo1968@icloud.com>
2026-05-01 04:51:59 -0400

    Add APIRouter instance to grade.py

--- routes/grade.py
+++ routes/grade.py
@@ -3,6 +3,7 @@ import csv
 import shutil
 import subprocess
 import importlib.util
+router = APIRouter()
 from datetime import datetime
 
 from fastapi import APIRouter, UploadFile, File, HTTPException
